69666bef7696e16ff03d9897552c32d13dfb0e7c
StackDoubleFlow <ojaslandge@gmail.com>
2020-10-26 08:01:18 -0500

    Fix copy and make

--- Android.mk
+++ Android.mk
@@ -29,14 +29,14 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := beatsaber-hook_0_7_8
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
 LOCAL_SRC_FILES := extern/libbeatsaber-hook_0_7_8.so
-LOCAL_CPP_FEATURES := exceptions
+LOCAL_CPP_FEATURES += exceptions
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: codegen - version: 0.3.1
 include $(CLEAR_VARS)
 LOCAL_MODULE := codegen_0_3_4
 LOCAL_EXPORT_C_INCLUDES := extern/codegen/include
 LOCAL_SRC_FILES := extern/libcodegen_0_3_4.so
-LOCAL_CPP_FEATURES := exceptions
+LOCAL_CPP_FEATURES += exceptions
 include $(PREBUILT_SHARED_LIBRARY)
 
 # If you would like to use more shared libraries (such as custom UI, utils, or more) add them here, following the format above.
